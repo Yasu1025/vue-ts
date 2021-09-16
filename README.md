6cc20b59d95eb2b710e1995c7cc6c2b4986b6371
Yasu1025 <sillyjp25@gmail.com>
2021-09-17 03:15:24 +0900

    【FEature】base

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # vue-ts
-Vue 3, Composition API, Vuex, Typescript, Docker, c3.js charts, Upload Images, Export CSV
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/pages/Users.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="table-responsive">
+    <table class="table table-striped table-sm">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Header</th>
+          <th>Header</th>
+          <th>Header</th>
+          <th>Header</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1,015</td>
+          <td>sodales</td>
+          <td>ligula</td>
+          <td>in</td>
+          <td>libero</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "users",
+};
+</script>
